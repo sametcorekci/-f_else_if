b4b675cc277af66e9a3b51c4f6793f82b73bbf63
sametcorekci <85850750+sametcorekci@users.noreply.github.com>
2022-04-25 01:38:36 +0300

    İf_else_if
    www.patika.dev

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int time=DateTime.Now.Hour;
+        //    Console.WriteLine(time);
+
+        //    if(time>=6 && time<11)
+        //         Console.WriteLine("Günaydın");
+
+        //    else if(time<=18)
+        //         Console.WriteLine("İyi günler");
+        //     else
+        //         // Console.WriteLine("İyi geceler");
+
+                string sonuc = time<=18 ? " İyi günler" :"İyi geceler"; // Yukardaki iflerin tek satırda yazılmasını sağlayan Ternary kod bloğu
+                sonuc = time>=6 && time<11 ? "Günaydın" : time<=18 ? " İyi günler" : "İyi geceler";
+                Console.WriteLine(sonuc);
+        }
+    }
+}
